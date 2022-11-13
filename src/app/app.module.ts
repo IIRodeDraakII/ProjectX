c7d87e46f7382696ecdd4b9372f471dfd2619e91
Jessey <jessey.de.man@viatim.nl>
2022-11-13 14:29:00 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainBoardComponent } from './mainBoard/mainBoard-item.component';
 import { SocialMediaComponent } from './boardItems/social-media/socialMedia-item.component';
+import { NavbarComponent } from './navbar/navbar-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainBoardComponent,
-    SocialMediaComponent
+    SocialMediaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
