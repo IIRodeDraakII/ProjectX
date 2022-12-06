d4379fcf732e4950e01caf8d813b4459b59e95d4
Jessey <jessey.de.man@viatim.nl>
2022-12-06 15:57:50 +0100

    generate component

---
+++ src/app/boardItems/showCV/showCV.item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-cv',
+  templateUrl: './showCV.item.component.html',
+  styleUrls: ['./showCV.item.component.scss']
+})
+export class ShowCVComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
